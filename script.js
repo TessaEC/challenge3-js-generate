78e8c62289dfc2a9a1954e36e55540638b42b270
TessaEC <118077000+TessaEC@users.noreply.github.com>
2022-12-21 12:09:14 -0600

    javascript updated with code

--- script.js
+++ script.js
@@ -1,7 +1,79 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Create const arrays - will not change:
+const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upper = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const specialCharacters = ["@", "%", "+", "\\", "/", ",", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."];
+
+
+// Enter empty array to be filled with const options:
+let passOption = []
+let passwordLength = 0;
+
+// Window prompts function:
+function prompts() {
+
+// Clear possible options:
+  passOption = [];
+
+// PROMPT - length with alerts:
+  let lengthPrompt = prompt("How many characters (between 8-128) do you need?");
+
+  if (isNaN(lengthPrompt)) {
+    alert("LENGTH MUST BE A NUMBER!");
+    return
+  } 
+
+  if (lengthPrompt < 8 || lengthPrompt > 128) {
+    alert("CHARACTER AMOUNT SHOULD BE BETWEEN 8 AND 128 CHARACTERS!");
+    return
+  }
+
+// Make string a number:
+  passwordLength = parseInt(lengthPrompt)
+
+// PROMPTS continued...
+  let numPrompt = confirm("Do you want to include numbers?")
+  let specialCharactersPrompt = confirm("Do you want to include special characters?")
+  let lowerPrompt = confirm("Do you want to include lower case letters")
+  let upperPrompt = confirm("Do you want to include upper case letters")
+
+// Push const:
+  if (numPrompt) passOption.push(...num);
+
+  if (specialCharactersPrompt) passOption.push(...specialCharacters);
+ 
+  if (lowerPrompt) passOption.push(...lower);
+
+  if (upperPrompt) passOption.push(...upper);
+
+}
+
+// Provided code - Create button for generate ID:
+var generateBtn = document.querySelector("#generate"); //add event listener with call back to prompt function(button calls prompt > prompt function calls generatePassword)
+
+// Display - function for generatePassword:
+function generatePassword() {
+
+//updates array of possible characters:
+  prompts();
+
+// create string from possible characters:
+  let password = "";
+
+//loop:
+  for(var i =0 ;i <passwordLength; i++){
+    //add random character from possOption to password
+    var index = Math.floor(Math.random() * passOption.length);
+    password += passOption[index];
+  }
+
+  return password;
+
+}
+
+// Provided code - Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +82,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Event listener for when button is clicked
 generateBtn.addEventListener("click", writePassword);
+
